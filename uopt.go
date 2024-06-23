941b6f313580cd292eb5023fb787333df70d5388
Jordan Ocokoljic <jordan@nofrills.software>
2024-06-24 00:56:17 +1000

    Rewrite to be a simpler option parser
    The old version did too much, and muddied the line
    between library and framework.

---
+++ uopt.go
@@ -0,0 +1,87 @@
+package uopt
+
+import "strings"
+
+type Visitor interface {
+	VisitArgument(argument string)
+	VisitFlag(flag string) (wasFlag bool)
+	VisitOption(option string, value string)
+}
+
+func Visit(visitor Visitor, arguments []string) {
+	visitOption := true
+
+	for i := 0; i < len(arguments); i++ {
+		arg := arguments[i]
+
+		if visitOption {
+			if arg == "--" {
+				visitOption = false
+				continue
+			}
+
+			if isLongOption(arg) {
+				idx := strings.Index(arg, "=")
+				if idx == -1 {
+					idx = len(arg)
+				}
+
+				if visitor.VisitFlag(arg[2:idx]) {
+					continue
+				}
+
+				if idx < len(arg) {
+					visitor.VisitOption(arg[2:idx], arg[idx+1:])
+					continue
+				}
+
+				var value string
+				if i+1 < len(arguments) && isOptionValue(arguments[i+1]) {
+					value = arguments[i+1]
+					i++
+				}
+
+				visitor.VisitOption(arg[2:], value)
+				continue
+			}
+
+			if isShortOption(arg) {
+				for j := 1; j < len(arg); j++ {
+					if visitor.VisitFlag(arg[j : j+1]) {
+						continue
+					}
+
+					if j >= len(arg)-1 {
+						if i+1 < len(arguments) && isOptionValue(arguments[i+1]) {
+							visitor.VisitOption(arg[j:j+1], arguments[i+1])
+							i++
+							continue
+						}
+
+						visitor.VisitOption(arg[j:j+1], "")
+						continue
+					}
+
+					visitor.VisitOption(arg[j:j+1], arg[j+1:])
+					break
+				}
+
+				continue
+			}
+		}
+
+		visitor.VisitArgument(arg)
+	}
+}
+
+func isLongOption(arg string) bool {
+	return strings.HasPrefix(arg, "--") && arg != "--"
+}
+
+func isShortOption(arg string) bool {
+	return strings.HasPrefix(arg, "-") && arg != "-"
+}
+
+func isOptionValue(arg string) bool {
+	return arg != "-" && !isLongOption(arg) && !isShortOption(arg)
+}
--- uopt_test.go
+++ uopt_test.go
@@ -2,85 +2,206 @@ package uopt_test
 
 import (
 	"github.com/jordanocokoljic/uopt"
+	"reflect"
 	"testing"
 )
 
-var benchResult uopt.Result
-var benchError error
+func TestVisit(t *testing.T) {
+	arguments := make([]string, 0)
+	flags := make([]string, 0)
+	options := make(map[string]string)
 
-var benchSchema = uopt.CommandSchema{
-	Options: []uopt.OptionSchema{
-		{
-			Name:  "help",
-			Short: "-h",
-			Long:  "--help",
+	visitor := Visitor{
+		Argument: func(s string) {
+			arguments = append(arguments, s)
 		},
-		{
-			Name: "version",
-			Long: "--version",
-		},
-		{
-			Name:  "verbose",
-			Short: "-v",
-			Long:  "--verbose",
-		},
-		{
-			Name:  "stream",
-			Short: "-s",
-			Long:  "--stream",
-		},
-		{
-			Name: "debug",
-			Long: "--debug",
-		},
-	},
-	Arguments: []string{
-		"src_file",
-		"out_file",
-		"log_file",
-		"debug_file",
-	},
-}
+		Flag: func(opt string) bool {
+			if 'A' <= opt[0] && opt[0] <= 'Z' {
+				return false
+			}
 
-var benchArguments = []string{
-	"--verbose",
-	"-s",
-	"--debug",
-	"in.file",
-	"out.file",
-	"log.file",
-	"debug_file",
-}
+			flags = append(flags, opt)
+			return true
+		},
+		Option: func(opt string, val string) {
+			options[opt] = val
+		},
+	}
 
-func Benchmark_ValidateBuildApply(b *testing.B) {
-	// Historically this produces about
-	// 1400~1500 ns/op, 1056 B/op, 7 allocs/op
+	args := []string{
+		"-a",
+		"-B", "something",
+		"-",
+		"-dbS", "5432",
+		"-Jfile.txt",
+		"--store",
+		"--Remove-from",
+		"temp",
+		"in.sql",
+		"--Notfound=error",
+		"--",
+		"-Z",
+		"--zero",
+	}
 
-	for i := 0; i < b.N; i++ {
-		benchError = benchSchema.Validate()
-		outline := benchSchema.Build()
-		benchResult, benchError = outline.ApplyTo(benchArguments)
+	expectedArguments := []string{
+		"-",
+		"in.sql",
+		"-Z",
+		"--zero",
+	}
+
+	expectedFlags := []string{
+		"a",
+		"d",
+		"b",
+		"store",
+	}
+
+	expectedOptions := map[string]string{
+		"B":           "something",
+		"S":           "5432",
+		"J":           "file.txt",
+		"Remove-from": "temp",
+		"Notfound":    "error",
+	}
+
+	uopt.Visit(visitor, args)
+
+	if !reflect.DeepEqual(expectedArguments, arguments) {
+		t.Errorf("got %v, want %v", arguments, expectedArguments)
+	}
+
+	if !reflect.DeepEqual(expectedFlags, flags) {
+		t.Errorf("got %v, want %v", flags, expectedFlags)
+	}
+
+	if !reflect.DeepEqual(expectedOptions, options) {
+		t.Errorf("got %v, want %v", options, expectedOptions)
 	}
 }
 
-func Benchmark_BuildApply(b *testing.B) {
-	// Historically this produces about
-	// 800~900 ns/op, 1056 B/op, 7 allocs/op
+func TestVisit_Arguments(t *testing.T) {
+	var collected []string
+	visitor := Visitor{
+		Argument: func(argument string) {
+			collected = append(collected, argument)
+		},
+	}
 
-	for i := 0; i < b.N; i++ {
-		outline := benchSchema.Build()
-		benchResult, benchError = outline.ApplyTo(benchArguments)
+	args := []string{"abc", "def", "--", "-"}
+	expected := []string{"abc", "def", "-"}
+
+	uopt.Visit(visitor, args)
+
+	if !reflect.DeepEqual(collected, expected) {
+		t.Errorf("got %v, want %v", expected, collected)
 	}
 }
 
-func Benchmark_Apply(b *testing.B) {
-	// Historically this produces about
-	// 400~500 ns/op, 672 B/op, 4 allocs/op
+func TestVisit_Flags(t *testing.T) {
+	var collected []string
+	visitor := Visitor{
+		Flag: func(option string) bool {
+			collected = append(collected, option)
+			return true
+		},
+		Argument: func(_ string) {},
+	}
 
-	outline := benchSchema.Build()
-	b.ResetTimer()
+	args := []string{"-abc", "-d", "--efg", "--", "-hij", "-k", "--lmn"}
+	expected := []string{"a", "b", "c", "d", "efg"}
 
-	for i := 0; i < b.N; i++ {
-		benchResult, benchError = outline.ApplyTo(benchArguments)
+	uopt.Visit(visitor, args)
+
+	if !reflect.DeepEqual(collected, expected) {
+		t.Errorf("got %v, want %v", collected, expected)
 	}
 }
+
+func TestVisit_Options(t *testing.T) {
+	tests := []struct {
+		name     string
+		args     []string
+		expected map[string]string
+	}{
+		{
+			name: "Basic",
+			args: []string{"-afile.txt", "-z", "image.png", "--in", "oneway", "--out=another", "--blank="},
+			expected: map[string]string{
+				"a":     "file.txt",
+				"z":     "image.png",
+				"in":    "oneway",
+				"out":   "another",
+				"blank": "",
+			},
+		},
+		{
+			name: "LongMissingValue",
+			args: []string{"--first", "--last"},
+			expected: map[string]string{
+				"first": "",
+				"last":  "",
+			},
+		},
+		{
+			name: "ShortMissingValue",
+			args: []string{"-a", "-z"},
+			expected: map[string]string{
+				"a": "",
+				"z": "",
+			},
+		},
+		{
+			name: "DashIsNotCaptured",
+			args: []string{"--first", "-", "-a", "-"},
+			expected: map[string]string{
+				"first": "",
+				"a":     "",
+			},
+		},
+		{
+			name: "ShortIgnoresDoubleDash",
+			args: []string{"-a", "--"},
+			expected: map[string]string{
+				"a": "",
+			},
+		},
+		{
+			name: "LongIgnoresDoubleDash",
+			args: []string{"--first", "--"},
+			expected: map[string]string{
+				"first": "",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			collected := make(map[string]string)
+			visitor := Visitor{
+				Flag:     func(_ string) bool { return false },
+				Argument: func(_ string) {},
+				Option: func(option string, value string) {
+					collected[option] = value
+				},
+			}
+
+			uopt.Visit(visitor, test.args)
+
+			if !reflect.DeepEqual(collected, test.expected) {
+				t.Errorf("got %v want %v", collected, test.expected)
+			}
+		})
+	}
+}
+
+type Visitor struct {
+	Argument func(string)
+	Flag     func(string) bool
+	Option   func(string, string)
+}
+
+func (v Visitor) VisitArgument(arg string)      { v.Argument(arg) }
+func (v Visitor) VisitFlag(arg string) bool     { return v.Flag(arg) }
+func (v Visitor) VisitOption(arg, value string) { v.Option(arg, value) }
